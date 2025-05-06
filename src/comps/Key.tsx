5154dff3be8ad8cd9aa761bdc0552d0d76d2fbd6
Iranigami <iranigami@mail.ru>
2025-05-06 16:52:39 +0400

    keyboard layoud changes

--- src/comps/Key.tsx
+++ src/comps/Key.tsx
@@ -1,24 +1,32 @@
 import backspace from "../assets/images/icons/backspace-inactive.svg";
 import globe from "../assets/images/icons/globe.svg";
-import shift from "../assets/images/icons/Shift Off.svg";
 
 type Props = {
   type: string;
   text: string;
   className?: string;
   clickHandler?: () => void;
+  shift?: number;
 };
 
-export default function Key({ type, text, className, clickHandler }: Props) {
+export default function Key({
+  type,
+  text,
+  className,
+  clickHandler,
+  shift,
+}: Props) {
   return (
     <button
-      className={`w-full h-full max-w-[158px] flex justify-center items-center text-center rounded-[8px] bg-[#FAFAFA] ${className} active:bg-[#E1E3E3] key-shadow font-montserrat`}
+      className={`h-[96px] flex justify-center items-center text-center rounded-[16px] text-[36px] bg-[#FAFAFA] ${className} active:bg-[#E1E3E3] shadow-[0px_2px_0px_0px_#0000004D] font-montserrat`}
       onClick={clickHandler}
     >
       {text}
       {type === "backspace" && <img src={backspace} alt="backspace" />}
       {type === "lang" && <img src={globe} alt="lang" />}
-      {type === "shift" && <img src={shift} alt="shift" />}
+      {type === "shift" && (
+        <img src={`src/assets/images/icons/Shift${shift}.svg`} alt="shift" />
+      )}
     </button>
   );
 }
--- src/comps/Keyboard.tsx
+++ src/comps/Keyboard.tsx
@@ -15,18 +15,29 @@ export default function Keyboard({
   onClose,
   onBackspace,
 }: Props) {
-  const [language, setLanguage] = useState("RUS");
-  const [uppercase, setUppercase] = useState(false);
+  const [language, setLanguage] = useState("ABC");
+  const [uppercase, setUppercase] = useState(0);
   const [isNumbersShown, setNumbersShown] = useState(false);
   return (
     <div
-      className={`transition ${opened && "translate-y-[-600px]"} duration-700 fixed absolute left-0 right-0 z-10 font-montserrat font-medium text-[18px]`}
+      className={`transition ${opened && "translate-y-[-2273px]"} bottom-[-2000px] duration-700 fixed absolute left-0 right-0 z-10 font-montserrat font-medium text-[18px]`}
     >
       <div
-        className={`w-[1840px] h-[652px] rounded-[48px] bg-white font-montserrat text-[#373737] justify-center items-center text-center px-[30.12px] pt-[24px]`}
+        className={`mx-auto w-[1840px] h-[652px] rounded-[48px] bg-white font-montserrat text-[#373737] justify-center items-center text-center px-[30.12px] pt-[48px] font-normal`}
       >
-        <div className="flex gap-[8px] justify-center items-center text-center mt-[24px]">
-          {language === "RUS" &&
+        <div className="mx-auto justify-center items-center w-full h-[76px] divide-x-2 flex divide-gray-third">
+          <div className="w-[276px] font-normal text-[36px] text-gray-primary leading-[100%] tracking-[-0.86px] text-center">
+            @gmail.com
+          </div>
+          <div className="w-[276px] font-normal text-[36px] text-gray-primary leading-[100%] tracking-[-0.86px] text-center">
+            @mail.ru
+          </div>
+          <div className="w-[276px] font-normal text-[36px] text-gray-primary leading-[100%] tracking-[-0.86px] text-center">
+            @yandex.ru
+          </div>
+        </div>
+        <div className="flex gap-[16px] justify-center items-center text-center mt-[48px]">
+          {language === "АБВ" &&
             !isNumbersShown &&
             ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ"].map(
               (text: string, index: number) => (
@@ -34,15 +45,15 @@ export default function Keyboard({
                   key={index}
                   text={uppercase ? text.toUpperCase() : text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(uppercase ? text.toUpperCase() : text);
-                    setUppercase(false);
+                    setUppercase(0);
                   }}
                 />
               ),
             )}
-          {language === "ENG" &&
+          {language === "ABC" &&
             !isNumbersShown &&
             ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"].map(
               (text: string, index: number) => (
@@ -50,10 +61,10 @@ export default function Keyboard({
                   key={index}
                   text={uppercase ? text.toUpperCase() : text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(uppercase ? text.toUpperCase() : text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
@@ -65,17 +76,17 @@ export default function Keyboard({
                   key={index}
                   text={text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
             )}
         </div>
-        <div className="flex gap-[10px] justify-center items-center text-center mt-[8px]">
-          {language === "RUS" &&
+        <div className="flex gap-[16px] justify-center items-center text-center mt-[16px]">
+          {language === "АБВ" &&
             !isNumbersShown &&
             ["ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э"].map(
               (text: string, index: number) => (
@@ -83,15 +94,15 @@ export default function Keyboard({
                   key={index}
                   text={uppercase ? text.toUpperCase() : text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(uppercase ? text.toUpperCase() : text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
             )}
-          {language === "ENG" &&
+          {language === "ABC" &&
             !isNumbersShown &&
             ["a", "s", "d", "f", "g", "h", "j", "k", "l"].map(
               (text: string, index: number) => (
@@ -99,38 +110,41 @@ export default function Keyboard({
                   key={index}
                   text={uppercase ? text.toUpperCase() : text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(uppercase ? text.toUpperCase() : text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
             )}
           {isNumbersShown &&
-            ["-", "/", ":", ";", "(", ")", "$", "&", "@", '"'].map(
+            ["$", "!", ";", "?", "=", "@", "(", ")"].map(
               (text: string, index: number) => (
                 <Key
                   key={index}
                   text={text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[158.5px]"
                   clickHandler={() => {
                     enterButton(text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
             )}
         </div>
-        <div className="flex gap-[10px] justify-center items-center text-center mt-[8px]">
+        <div className="flex gap-[16px] justify-center items-center text-center mt-[16px]">
           <Key
+            shift={uppercase}
             text=""
             type="shift"
-            className="bg-keyboard-gray w-[104px]"
-            clickHandler={() => setUppercase(!uppercase)}
+            className={`${!uppercase && "bg-blue-bg active:bg-light-blue"} w-[208px]`}
+            clickHandler={() =>
+              setUppercase(uppercase !== 2 ? uppercase + 1 : 0)
+            }
           />
-          {language === "RUS" &&
+          {language === "АБВ" &&
             !isNumbersShown &&
             ["я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "ё"].map(
               (text: string, index: number) => (
@@ -138,15 +152,15 @@ export default function Keyboard({
                   key={index}
                   text={uppercase ? text.toUpperCase() : text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(uppercase ? text.toUpperCase() : text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
             )}
-          {language === "ENG" &&
+          {language === "ABC" &&
             !isNumbersShown &&
             ["z", "x", "c", "v", "b", "n", "m"].map(
               (text: string, index: number) => (
@@ -154,71 +168,65 @@ export default function Keyboard({
                   key={index}
                   text={uppercase ? text.toUpperCase() : text}
                   type="symbol"
-                  className="w-[65px]"
+                  className="w-[130px]"
                   clickHandler={() => {
                     enterButton(uppercase ? text.toUpperCase() : text);
-                    setUppercase(false);
+                    if (uppercase !== 2) setUppercase(0);
                   }}
                 />
               ),
             )}
           {isNumbersShown &&
-            [".", ",", "?", "!", "'"].map((text: string, index: number) => (
+            [".", "_", "-", "+"].map((text: string, index: number) => (
               <Key
                 key={index}
                 text={text}
                 type="symbol"
-                className="w-[65px]"
+                className="w-[237.5px]"
                 clickHandler={() => {
                   enterButton(text);
-                  setUppercase(false);
+                  if (uppercase !== 2) setUppercase(0);
                 }}
               />
             ))}
           <Key
             text={""}
             type="backspace"
-            className="w-[103px] bg-keyboard-gray"
+            className="w-[206px] bg-blue-bg active:bg-light-blue"
             clickHandler={() => {
               onBackspace();
             }}
           />
         </div>
-        <div className="flex gap-[10px] justify-center items-center text-center mt-[12.44px]">
+        <div className="flex gap-[16px] justify-center items-center text-center mt-[16px] font-medium">
           <Key
-            text={"&123"}
+            text={!isNumbersShown ? "&123" : language}
             type="nums"
-            className="w-[110px] bg-keyboard-gray"
+            className="w-[280px] bg-blue-bg active:bg-light-blue"
             clickHandler={() => setNumbersShown(!isNumbersShown)}
+          />
+          <Key
+            text={language === "ABC" ? "Space" : "Пробел"}
+            type="symbol"
+            className={`${language === "АБВ" && !isNumbersShown ? "w-full" : "w-[852px]"} font-medium`}
+            clickHandler={() => {
+              enterButton(" ");
+              if (uppercase !== 2) setUppercase(0);
+            }}
           />
           <Key
             text=""
             type="lang"
-            className="w-[110px] bg-keyboard-gray"
+            className="w-[280px] bg-blue-bg active:bg-light-blue"
             clickHandler={() => {
-              setLanguage(`${language === "RUS" ? "ENG" : "RUS"}`);
+              setLanguage(`${language === "АБВ" ? "ABC" : "АБВ"}`);
             }}
-          />
-          <Key
-            text={"Пробел"}
-            type="symbol"
-            className="w-[552px]"
-            clickHandler={() => {
-              enterButton(" ");
-              setUppercase(false);
-            }}
-          />
-          <Key
-            text={"Ввод"}
-            type="enter"
-            className="bg-red text-white w-[152px]"
-            clickHandler={()=>{}}
           />
         </div>
       </div>
       <div
         onClick={onClose}
-        className="mt-[20px] w-[88px] h-[56px] rounded-[24px] bg-white ml-[416px] flex justify-center items-center"
+        className="mt-[40px] w-[176px] h-[112px] rounded-[48px] bg-white mx-auto flex justify-center items-center"
       >
         <img src={close} alt="close" className="mx-auto" />
       </div>
