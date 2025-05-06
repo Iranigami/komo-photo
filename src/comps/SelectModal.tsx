0bd654e66bbea6112ff9fe0a72822092760ea045
Iranigami <iranigami@mail.ru>
2025-05-06 16:53:52 +0400

    WIP

--- src/comps/SelectModal.tsx
+++ src/comps/SelectModal.tsx
@@ -1,36 +1,55 @@
-import { useState } from "react"
-import type { VariantData } from "../Types"
-import next from "../assets/images/icons/next.svg"
+import { useState } from "react";
+import type { VariantData } from "../Types";
+import next from "../assets/images/icons/next.svg";
+import back from "../assets/images/icons/blue next.svg";
 
 type Props = {
-    data: VariantData[],
-}
+  data: VariantData[];
+  texts: string[];
+  onSelect: (id: number) => void;
+  onNext?: () => void;
+  onBack?: () => void;
+};
 
-export default function SelectModal({data}: Props){
-    const [selected, setSelected] = useState(0);
-    return(
-        <div className="w-full bottom-0 pt-[64px] pb-[128px] bg-white fixed gap-[64px] rounded-t-[136px] px-[64px]">
-            <div className="text-gray-primary text-[80px] tracking-0 leading-[100%] uppercase text-left font-bold font-osnova-pro">
-                Персонажи
-            </div>
-            <div className={"w-[2032px] h-[874px] mt-[32px] bg-black"}>
-
-            </div>
-            <div className="mt-[64px] font-osnova-pro font-bold text-[56px] leading-[100%] tracking-0 text-gray-primary">
-                {data[selected].title}
-            </div>
-            <div className="mt-[32px] font-osnova-pro font-normal text-[40px] leading-[100%] tracking-0 text-gray-secondary">
-                {data[selected].description}
-            </div>
-            <div className="w-full mt-[64px] flex justify-end gap-[64px] items-center h-[146px]">
-                <div className="w-[520px] flex gap-[16px] font-osnova-pro font-normal leading-[100%] tracking-0 text-[40px] text-gray-secondary">
-                    Перейти к выбору фона
-                    <img src={next} alt="next step" className="size-[48px]" />
-                </div>
-                <button className="w-[461px] h-[146px] rounded-[72px] bg-blue-accent flex justify-center items-center font-osnova-pro font-bold text-[48px] text-white leading-[100%] tracking-0 text-center">
-                    Продолжить
-                </button>
-            </div>
+export default function SelectModal({
+  data,
+  texts,
+  onSelect,
+  onNext,
+  onBack,
+}: Props) {
+  const [selected, setSelected] = useState(0);
+  return (
+    <div className="w-full bottom-0 pt-[64px] pb-[128px] bg-white fixed gap-[64px] rounded-t-[136px] px-[64px]">
+      <div className="text-gray-primary text-[80px] tracking-0 leading-[100%] uppercase text-left font-bold font-osnova-pro">
+        {texts[0]}
+      </div>
+      <div className={"w-[2032px] h-[874px] mt-[32px] bg-black"}></div>
+      <div className="mt-[64px] font-osnova-pro font-bold text-[56px] leading-[100%] tracking-0 text-gray-primary">
+        {data[selected].title}
+      </div>
+      <div className="mt-[32px] font-osnova-pro font-normal text-[40px] leading-[100%] tracking-0 text-gray-secondary">
+        {data[selected].description}
+      </div>
+      <div className="w-full mt-[64px] flex justify-end gap-[64px] items-center h-[146px]">
+        <button
+          hidden={!texts[2]}
+          className="w-[378px] h-[146px] border-[4px] border-blue-accent rounded-[72px] mr-[662px] gap-[24px] font-osnova-pro font-bold text-[48px] leading-[100%] tracking-0 flex justify-center items-center text-blue-accent"
+        >
+          <img src={back} alt="back" className="size-[48px] rotate-180" />
+          {texts[2]}
+        </button>
+        <div className="w-[520px] flex gap-[16px] font-osnova-pro font-normal leading-[100%] tracking-0 text-[40px] text-gray-secondary">
+          {texts[1]}
+          <img src={next} alt="next step" className="size-[48px]" />
         </div>
-    )
+        <button
+          onClick={onNext}
+          className="w-[461px] h-[146px] rounded-[72px] bg-blue-accent flex justify-center items-center font-osnova-pro font-bold text-[48px] text-white leading-[100%] tracking-0 text-center"
+        >
+          Продолжить
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,10 +1,153 @@
-import Webcam from "react-webcam"
-import Keyboard from "../comps/Keyboard"
+import Webcam from "react-webcam";
+import frame from "../assets/images/frame.svg";
+import sparkles from "../assets/images/sparkles.gif";
+import time from "../assets/images/icons/timer.svg";
+import React, { useRef, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Camera() {
-    return(
-        <div>
-            <Keyboard opened={true} onClose={()=>{}} enterButton={()=>{}} onBackspace={()=>{}}/>
+  const [isSnapButtonDisabled, setSnapButtonDisabled] = useState(false);
+  const searchParams = useSearchParams();
+  const navigate = useNavigate();
+  const [isErrorStated, setErrorStated] = useState(false);
+  const [timerAppearance, setTimerAppearance] = useState(0);
+  const timer = useRef(3000);
+  const webcamRef = React.useRef<Webcam>(null);
+  const makePhoto = () => {
+    setSnapButtonDisabled(true);
+    if (!webcamRef.current) console.log("Error");
+    else
+      setTimeout(() => {
+        capture();
+      }, timer.current);
+  };
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current!.getScreenshot();
+    //тут идет отправка
+    navigate("/result"); //тут добавить парамс для получения картинки
+  }, [webcamRef]);
+  const videoConstraints = {
+    width: 3840,
+    height: 2160,
+    facingMode: "user",
+  };
+  return (
+    <div className="fixed top-0 w-full h-full">
+      <div className="mt-[128px] text-white text-[240px] tracking-[-9.6px] leading-[100%] uppercase text-center font-bold font-osnova-pro">
+        Поместите лицо
+        <br />в область
+      </div>
+      <img src={frame} alt="here" className="mt-[197px] mx-auto z-100" />
+      <img
+        src={sparkles}
+        alt="img"
+        className="absolute left-0 right-0 mx-auto mt-[-1200px] z-100"
+      />
+      <Webcam
+        onUserMediaError={() => setErrorStated(true)}
+        screenshotFormat="image/jpeg"
+        audio={false}
+        ref={webcamRef}
+        width={3840}
+        height={2160}
+        videoConstraints={videoConstraints}
+        className="top-[600px] rotate-90 z-[-1] absolute scale-[230%]"
+      />
+      <div className="w-full h-[360px] rounded-t-[136px] bg-white bottom-0 fixed px-[64px] flex justify-between items-center">
+        <div className="flex justify-center items-center gap-[64px]">
+          <div className="flex gap-[32px] justify-center items-center">
+            <img src={time} alt="timer" className="size-[48px]" />
+            <div className="font-osnova-pro font-normal text-gray-secondary leading-[100%] tracking-0 text-[40px]">
+              Таймер
+            </div>
+          </div>
+          <div className="p-[4px] relative bg-blue-bg w-[840px] h-[114px] rounded-[44px] flex justify-left items-center font-osnova-pro text-[40px] text-center leading-[100%] tracking-0">
+            <div
+              onClick={() => {
+                setTimerAppearance(0);
+                timer.current = 3000;
+              }}
+              className={`${timerAppearance === 0 ? "text-white font-bold" : "text-gray-secondary font-normal"} items-center flex justify-center w-[208px] h-[106px] z-1`}
+            >
+              3 c
+            </div>
+            <div
+              onClick={() => {
+                setTimerAppearance(1);
+                timer.current = 5000;
+              }}
+              className={`${timerAppearance === 1 ? "text-white font-bold" : "text-gray-secondary font-normal"} items-center flex justify-center w-[208px] h-[106px] z-1`}
+            >
+              5 c
+            </div>
+            <div
+              onClick={() => {
+                setTimerAppearance(2);
+                timer.current = 7000;
+              }}
+              className={`${timerAppearance === 2 ? "text-white font-bold" : "text-gray-secondary font-normal"} items-center flex justify-center w-[208px] h-[106px] z-1`}
+            >
+              7 c
+            </div>
+            <div
+              onClick={() => {
+                setTimerAppearance(3);
+                timer.current = 10000;
+              }}
+              className={`${timerAppearance === 3 ? "text-white font-bold" : "text-gray-secondary font-normal"} items-center flex justify-center w-[208px] h-[106px] z-1`}
+            >
+              10 c
+            </div>
+            <div
+              style={{
+                transform: `translateX(${timerAppearance * 208}px)`,
+              }}
+              className={`bg-blue-accent absolute w-[208px] h-[106px] rounded-[40px] duration-150 z-0`}
+            />
+          </div>
         </div>
-    )
+        <button
+          disabled={isSnapButtonDisabled}
+          onClick={makePhoto}
+          className="disabled:opacity-[50%] w-[664px] h-[168px] rounded-[72px] bg-blue-accent flex justify-center items-center text-white font-osnova-pro font-bold leading-[100%] tracking-0 text-[48px]"
+        >
+          Сделать фотографию
+        </button>
+      </div>
+      <div
+        hidden={!isErrorStated}
+        className="fixed top-0 w-[100vw] h-[100vh] bg-[#000000DD] z-1000 content-center"
+      >
+        <div className="w-[1500px] bg-white mx-auto rounded-[70px] font-osnova-pro text-gray-primary py-[150px]">
+          <div className="font-bold text-[80px] text-center uppercase">
+            Не удалось найти камеру
+          </div>
+          <div className="font-normal text-[60px] text-center mt-[32px]">
+            Попробуйте следующее:
+            <br />
+            1. Проверьте, подключена ли веб-камера
+            <br />
+            2. Проверьте, даны ли соответствующие разрешения приложению
+            <br />
+            <br />
+            После выполнения этих шагов, перезагрузите страницу
+          </div>
+          <div className="flex justify-center items-center gap-[64px] mt-[64px]">
+            <button
+              onClick={() => navigate("/")}
+              className="w-[664px] h-[168px] rounded-[72px] border-4 border-blue-accent flex justify-center items-center text-blue-accent font-osnova-pro font-bold leading-[100%] tracking-0 text-[48px]"
+            >
+              На главную
+            </button>
+            <button
+              onClick={() => window.location.reload()}
+              className="w-[664px] h-[168px] rounded-[72px] bg-blue-accent flex justify-center items-center text-white font-osnova-pro font-bold leading-[100%] tracking-0 text-[48px]"
+            >
+              Перезагрузить
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
